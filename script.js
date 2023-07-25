12fffa518f4c9fbe66b05a7e7c14b33c3a70ae70
trohith89 <75958140+trohith89@users.noreply.github.com>
2023-07-25 08:57:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,69 @@
-//complete this code
-class Animal {}
+class Animal {
+  constructor(species) {
+    this._species = species;
+  }
 
-class Dog extends Animal {}
+  // Getter for species
+  get species() {
+    return this._species;
+  }
 
-class Cat extends Animal {}
+  // Method to make a sound
+  makeSound() {
+    console.log("Unknown sound.");
+  }
+}
 
+class Cat extends Animal {
+  constructor() {
+    // Since Cat is a subclass of Animal, we call the parent class constructor using super
+    super("Cat");
+  }
+
+  // Method specific to Cat class
+  purr() {
+    console.log("purr");
+  }
+
+  // Override the makeSound method for Cat
+  makeSound() {
+    console.log("Meow!");
+  }
+}
+
+class Dog extends Animal {
+  constructor() {
+    // Since Dog is a subclass of Animal, we call the parent class constructor using super
+    super("Dog");
+  }
+
+  // Method specific to Dog class
+  bark() {
+    console.log("Woof!");
+  }
+
+  // Override the makeSound method for Dog
+  makeSound() {
+    console.log("Bark!");
+  }
+}
+
+// Creating instances of Animal, Cat, and Dog
+const animal = new Animal("Unknown");
+const cat = new Cat();
+const dog = new Dog();
+
+// Using getters and methods
+console.log(animal.species); // Output: Unknown
+console.log(cat.species); // Output: Cat
+console.log(dog.species); // Output: Dog
+
+animal.makeSound(); // Output: Unknown sound.
+cat.makeSound();    // Output: Meow!
+dog.makeSound();    // Output: Bark!
+
+cat.purr(); // Output: purr
+dog.bark(); // Output: Woof
 // Do not change the code below this line
 window.Animal = Animal;
 window.Dog = Dog;

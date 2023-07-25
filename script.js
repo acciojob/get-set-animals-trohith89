cb29d85d081602cfc4c3e6f89338c6842915c4b0
trohith89 <75958140+trohith89@users.noreply.github.com>
2023-07-25 14:54:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,67 +3,38 @@ class Animal {
     this._species = species;
   }
 
-  // Getter for species
   get species() {
     return this._species;
   }
 
-  // Method to make a sound
   makeSound() {
-    console.log("Unknown sound.");
+    console.log("Some generic sound");
   }
 }
 
 class Cat extends Animal {
-  constructor() {
-    // Since Cat is a subclass of Animal, we call the parent class constructor using super
-    super("Cat");
-  }
-
-  // Method specific to Cat class
   purr() {
     console.log("purr");
-  }
-
-  // Override the makeSound method for Cat
-  makeSound() {
-    console.log("Meow!");
   }
 }
 
 class Dog extends Animal {
-  constructor() {
-    // Since Dog is a subclass of Animal, we call the parent class constructor using super
-    super("Dog");
-  }
-
-  // Method specific to Dog class
   bark() {
-    console.log("Woof!");
-  }
-
-  // Override the makeSound method for Dog
-  makeSound() {
-    console.log("Bark!");
+    console.log("woof");
   }
 }
 
-// Creating instances of Animal, Cat, and Dog
-const animal = new Animal("Unknown");
-const cat = new Cat();
-const dog = new Dog();
+// Example usage:
+const cat = new Cat("Cat");
+const dog = new Dog("Dog");
 
-// Using getters and methods
-console.log(animal.species); // Output: Unknown
-console.log(cat.species); // Output: Cat
-console.log(dog.species); // Output: Dog
+console.log(cat.species); // Output: "Cat"
+cat.makeSound(); // Output: "Some generic sound" (inherited from Animal)
+cat.purr(); // Output: "purr"
 
-animal.makeSound(); // Output: Unknown sound.
-cat.makeSound();    // Output: Meow!
-dog.makeSound();    // Output: Bark!
-
-cat.purr(); // Output: purr
-dog.bark(); // Output: Woof
+console.log(dog.species); // Output: "Dog"
+dog.makeSound(); // Output: "Some generic sound" (inherited from Animal)
+dog.bark(); // Output: "woof"
 // Do not change the code below this line
 window.Animal = Animal;
 window.Dog = Dog;
